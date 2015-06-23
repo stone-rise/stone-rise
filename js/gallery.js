2f6d5b476832f40f58ec31233def75898272ecec
taiti <taiti@stone-rise.com>
2015-06-23 17:42:46 +0900

    grallery page top back button created refs #7

---
+++ js/gallery.js
@@ -0,0 +1,12 @@
+// grallery.js
+// 20150623 taiti
+// 
+$(function () {
+    $('.pics').glisse({
+        changeSpeed: 550, 
+        speed: 500,
+        effect:'rotate',
+        fullscreen: false,
+        disablindRightClick: false
+    }); 
+});
