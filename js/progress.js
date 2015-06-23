63446bf0f34c74cd56fb2ee2f2fbe5ba15ba9c3f
taiti <taiti@stone-rise.com>
2015-06-23 16:52:57 +0900

    profile page created refs #8

---
+++ js/progress.js
@@ -0,0 +1,117 @@
+// progressbar jQuery
+// 20150623 taiti
+
+//.html progress
+$(function() {
+    $(document).ready(function() {
+        $('.html_skill').css({'width':'80%'});
+    });
+});
+
+//.css progress
+$(function() {
+    $(document).ready(function() {
+        $('.css_skill').css({'width':'80%'});
+    });
+});
+
+//.jquery progress
+$(function() {
+    $(document).ready(function() {
+        $('.jquery_skill').css({'width':'40%'});
+    });
+});
+
+//.bootstrap progress
+$(function() {
+    $(document).ready(function() {
+        $('.bootstrap_skill').css({'width':'50%'});
+    });
+});
+
+//.linux progress
+$(function() {
+    $(document).ready(function() {
+        $('.linux_skill').css({'width':'30%'});
+    });
+});
+
+//.vagrant progress
+$(function() {
+    $(document).ready(function() {
+        $('.vagrant_skill').css({'width':'45%'});
+    });
+});
+
+//.javascript progress
+$(function() {
+    $(document).ready(function() {
+        $('.javascript_skill').css({'width':'20%'});
+    });
+});
+
+//.php progress
+$(function() {
+    $(document).ready(function() {
+        $('.php_skill').css({'width':'15%'});
+    });
+});
+
+//.rails progress
+$(function() {
+    $(document).ready(function() {
+        $('.rails_skill').css({'width':'18%'});
+    });
+});
+
+//.vps progress
+$(function() {
+    $(document).ready(function() {
+        $('.vps_skill').css({'width':'50%'});
+    });
+});
+
+//.mysql progress
+$(function() {
+    $(document).ready(function() {
+        $('.mysql_skill').css({'width':'48%'});
+    });
+});
+
+//.git progress
+$(function() {
+    $(document).ready(function() {
+        $('.git_skill').css({'width':'58%'});
+    });
+});
+
+//.photoshop progress
+$(function() {
+    $(document).ready(function() {
+        $('.photoshop_skill').css({'width':'75%'});
+    });
+});
+
+//.illustrator progress
+$(function() {
+    $(document).ready(function() {
+        $('.illustrator_skill').css({'width':'56%'});
+    });
+});
+
+//.flash progress
+$(function() {
+    $(document).ready(function() {
+        $('.flash_skill').css({'width':'12%'});
+    });
+});
+
+//.sai progress
+$(function() {
+    $(document).ready(function() {
+        $('.sai_skill').css({'width':'80%'});
+    });
+});
+
+
+
